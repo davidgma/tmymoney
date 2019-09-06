02708f2ebf1aad5c7944947421909ab36afdb3eb
david ma <dmlh02@gmail.com>
2019-09-06 18:26:42 +0100

    made home directory a variable

--- tmymoney/index.ts
+++ tmymoney/index.ts
@@ -1,19 +1,18 @@
 import { Logger } from './logger';
 import { FileUtils } from './file-utils';
 import { readFile } from 'fs';
+import { homedir} from 'os';
 
-const logDir = "/home/david/.local/tmymoney";
+const logDir = homedir + "/.local/tmymoney";
 const logName = "tmymoney.log";
 
 async function main() {
     let logger = new Logger(logDir, logName);
-
+    console.log("homedir: " + homedir);
     console.log("Converting the KMyMoney file...");
     let command = "./convert-xml";
     let args = new Array<string>();
     // args.push("../../../kmymoney/KMyMoney_David.xml");
-    // args.push(">");
-    // args.push("../test.json");
     FileUtils.execute(command, args).then((res) => {
         for (let line of res)
             console.log(line);
